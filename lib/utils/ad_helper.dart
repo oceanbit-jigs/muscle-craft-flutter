8e159874f441b82732dbd60aa01bef8b80ffab17
milin2311 <milinpatel2311@gmail.com>
2026-03-02 15:32:31 +0530

    - add real admob ids for release

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -1,20 +1,37 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 class AdHelper {
+  static bool get isDebug => kDebugMode;
+
   static String get bannerAdUnitId {
+    if (isDebug) {
+      if (Platform.isAndroid) {
+        return 'ca-app-pub-3940256099942544/6300978111';
+      } else if (Platform.isIOS) {
+        return 'ca-app-pub-3940256099942544/2934735716';
+      }
+    }
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-8499191067388588/9757524063';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-8499191067388588/4775137612';
     }
     throw UnsupportedError('Unsupported platform');
   }
 
   static String get interstitialAdUnitId {
+    if (isDebug) {
+      if (Platform.isAndroid) {
+        return 'ca-app-pub-3940256099942544/1033173712';
+      } else if (Platform.isIOS) {
+        return 'ca-app-pub-3940256099942544/4411468910';
+      }
+    }
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return 'ca-app-pub-8499191067388588/3064957351';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/4411468910';
+      return 'ca-app-pub-8499191067388588/4761182401';
     }
     throw UnsupportedError('Unsupported platform');
   }
